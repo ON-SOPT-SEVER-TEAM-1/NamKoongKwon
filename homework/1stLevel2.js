2d981826e07668b710339884225c3d8ed391803f
kkoon9 <rndrnjs2003@gmail.com>
2020-10-11 17:04:22 +0900

    fix : Update 1st Seminar Level2 homework
    - 세미나 팀원들의 정보를 출력하는 함수를 만들자.

--- homework/1stLevel2.js
+++ homework/1stLevel2.js
@@ -1,38 +1,64 @@
-const Server세미나1조 = [
+const ServerSeminar1 = [
   {
     name: "남궁권",
     part: "Server",
     status: "OB",
-    gender: "남"
+    gender: "남",
+    사는곳: "부천",
+    나이: "26",
+    취미: "산책"
   },
   {
     name: "석영현",
     part: "Server",
     status: "OB",
-    gender: "여"
+    gender: "여",
+    사는곳: "부천",
+    나이: "26",
+    취미: "산책"
   },
   {
     name: "이현주",
     part: "Server",
     status: "OB",
-    gender: "여"
+    gender: "여",
+    사는곳: "부천",
+    나이: "24",
+    취미: "산책"
   },
   {
     name: "신지혜",
     part: "Server",
     status: "YB",
-    gender: "여"
+    gender: "여",
+    사는곳: "북한산",
+    나이: "23",
+    취미: "산책"
   },
   {
     name: "이수진",
     part: "Server",
     status: "OB",
-    gender: "여"
+    gender: "여",
+    사는곳: "부천",
+    나이: "26",
+    취미: "산책"
   },
   {
     name: "양재욱",
     part: "Server",
     status: "OB",
-    gender: "남"
+    gender: "남",
+    사는곳: "부천",
+    나이: "26",
+    취미: "산책"
   }
 ]
+
+const getMemberList = (members) => {
+  members.filter(member => {
+    console.log(member);
+  });
+}
+
+getMemberList(ServerSeminar1);
