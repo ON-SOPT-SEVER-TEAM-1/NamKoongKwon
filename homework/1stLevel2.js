93bd151dc9ba187a5b2b52b89ad6056a32710d33
kkoon9 <rndrnjs2003@gmail.com>
2020-10-11 16:51:15 +0900

    feat : Add 1st Seminar Level2 homework
    - 세미나 팀원들의 정보를 Json Array로 선언하자.

---
+++ homework/1stLevel2.js
@@ -0,0 +1,38 @@
+const Server세미나1조 = [
+  {
+    name: "남궁권",
+    part: "Server",
+    status: "OB",
+    gender: "남"
+  },
+  {
+    name: "석영현",
+    part: "Server",
+    status: "OB",
+    gender: "여"
+  },
+  {
+    name: "이현주",
+    part: "Server",
+    status: "OB",
+    gender: "여"
+  },
+  {
+    name: "신지혜",
+    part: "Server",
+    status: "YB",
+    gender: "여"
+  },
+  {
+    name: "이수진",
+    part: "Server",
+    status: "OB",
+    gender: "여"
+  },
+  {
+    name: "양재욱",
+    part: "Server",
+    status: "OB",
+    gender: "남"
+  }
+]
