85ebbd258077a21db21aabc488594c6f87849dad
kkoon9 <rndrnjs2003@gmail.com>
2020-10-13 11:43:57 +0900

    style: fix magic number
    - 매직 넘버(6)을 cosnt 변수로 만들어 상수 취급해주었다.

--- homework/1stLevel3/1stLevel3.js
+++ homework/1stLevel3/1stLevel3.js
@@ -3,7 +3,7 @@ const groupMixer = require('./modules/groupMixer');
 const groupPrint = require('./modules/groupPrint');
 const divideStatus = require('./modules/getIndex');
 const group = [];
-
+const groupCount = 6;
 const memberIndex = divideStatus(members); // 배열을 리턴
 const OBidx = memberIndex[0];
 const YBidx = memberIndex[1];
@@ -11,21 +11,21 @@ const YBidx = memberIndex[1];
 const mixedOBidx = groupMixer.mix(OBidx); // OB 15명
 const mixedYBidx = groupMixer.mix(YBidx); // YB 21명
 
-for (let i = 0; i < 6; i++) {
+for (let i = 0; i < groupCount; i++) {
   group.push({ teamNumber: `${i + 1}조`, teamMember: [] });
 }
 
 let index = 0;
 mixedOBidx.map((idx) => {
   group[index++].teamMember.push(members[idx]);
-  if (index == 6) {
+  if (index == groupCount) {
     index = 0;
   }
 });
 
 mixedYBidx.map((idx) => {
   group[index++].teamMember.push(members[idx]);
-  if (index == 6) {
+  if (index == groupCount) {
     index = 0;
   }
 });
