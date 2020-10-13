ca96aaad9bd3b525ef39be3261d2fce2b05ec289
kkoon9 <rndrnjs2003@gmail.com>
2020-10-13 10:05:18 +0900

    fix : update level2 homework
    - 팀원들 정보 수정

--- homework/1stLevel2.js
+++ homework/1stLevel2.js
@@ -1,4 +1,4 @@
-const ServerSeminar1 = [
+const teamMember = [
   {
     name: "남궁권",
     part: "Server",
@@ -13,25 +13,25 @@ const ServerSeminar1 = [
     part: "Server",
     status: "OB",
     gender: "여",
-    사는곳: "부천",
-    나이: "26",
-    취미: "산책"
+    사는곳: "동작구 상도동",
+    나이: "23",
+    취미: "낮잠"
   },
   {
     name: "이현주",
     part: "Server",
     status: "OB",
     gender: "여",
-    사는곳: "부천",
+    사는곳: "용산구 청파동",
     나이: "24",
-    취미: "산책"
+    취미: "넷플릭스 ㅎㅅㅎ"
   },
   {
     name: "신지혜",
     part: "Server",
     status: "YB",
     gender: "여",
-    사는곳: "북한산",
+    사는곳: "쌍문역",
     나이: "23",
     취미: "산책"
   },
@@ -40,20 +40,20 @@ const ServerSeminar1 = [
     part: "Server",
     status: "OB",
     gender: "여",
-    사는곳: "부천",
-    나이: "26",
-    취미: "산책"
+    사는곳: "고양시",
+    나이: "23",
+    취미: "닌텐도 게임"
   },
   {
     name: "양재욱",
     part: "Server",
     status: "OB",
     gender: "남",
-    사는곳: "부천",
-    나이: "26",
-    취미: "산책"
+    사는곳: "서초구 잠원동",
+    나이: "25",
+    취미: "운동하기"
   }
-]
+];
 
 const getMemberList = (members) => {
   members.filter(member => {
@@ -61,4 +61,4 @@ const getMemberList = (members) => {
   });
 }
 
-getMemberList(ServerSeminar1);
+getMemberList(teamMember);
